efbfe7ae4e7e3c8f5ecdf45329a6a2db895a7b0f
Andreas <andrnors@stud.ntnu.no>
2018-05-01 16:19:01 +0200

    push basic data works

--- src/components/PostView.vue
+++ src/components/PostView.vue
@@ -3,17 +3,17 @@
     <div class="mdl-grid">
       <div class="mdl-cell mdl-cell--8-col">
         <div class="card-image__picture">
-          <img :src="this.catUrl"/>
+          <img src="https://static.regnr.info/bilder/0/3/thumb_034eb7a2153f3a87b7c1136fe0dae835.jpg"/>
         </div>
       </div>
       <div class="mdl-cell mdl-cell--4-col mdl-cell--8-col-tablet">
         <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label is-upgraded is-dirty">
-          <input id="username" v-model="title" type="text" class="mdl-textfield__input"/>
+          <input id="username" type="text" class="mdl-textfield__input"/>
           <label for="username" class="mdl-textfield__label">Describe me</label>
         </div>
         <div class="actions">
-          <a @click.prevent="postCat" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
-            POST A CAT
+          <a @click.prevent="postCar" class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored">
+            Legg til bil
           </a>
         </div>
       </div>
@@ -29,8 +29,13 @@ export default {
   },
   methods: {
     postCar(){
-      this.$root.firebaseRefs.cat.push(
-        {'carobjet':'car'}
+      console.log(this.$root.$firebaseRefs)
+      this.$root.$firebaseRefs.cars.push(
+        {
+          "picture": "https://static.regnr.info/bilder/0/3/thumb_034eb7a2153f3a87b7c1136fe0dae835.jpg",          
+          'registreingsnummer': "DL12345",
+          'type': "Testbil123 MERCEDES"
+        }
       ).then(this.$router.push("/"));
     }
   }
